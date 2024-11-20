8a2f7dc3310c5e5426ecdd7d012a476e0c1e85cf
Peace Nnorom <nnorompeace14@gmail.com>
2024-11-20 14:52:06 +0100

    registration endpoint

---
+++ ServerSide/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/dbconns');
+const authRoutes = require('./routes/userAuth');
+const cors = require('cors');
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+
+const app = express();
+
+app.use(cors({
+    origin: '*' 
+  })); 
+
+// Middleware
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
